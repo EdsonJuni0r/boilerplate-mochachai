2e99f4dd96ea0c3a3831c7efd58a2f5166e49096
EdsonJuni0r <vianaedsonjunior@gmail.com>
2026-01-02 13:18:44 -0400

    Update functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -16,8 +16,8 @@ suite('Functional Tests', function () {
         .keepOpen()
         .get('/hello')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello Guest');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Guest');
           done();
         });
     });
@@ -26,10 +26,10 @@ suite('Functional Tests', function () {
       chai
         .request(server)
         .keepOpen()
-        .get('/hello?name=xy_z')
+        .get('/hello?name=Edson')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello xy_z');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Edson');
           done();
         });
     });
@@ -40,27 +40,50 @@ suite('Functional Tests', function () {
         .keepOpen()
         .put('/travellers')
 
+        .send({ surname: 'Colombo' })
+
         .end(function (err, res) {
-          assert.fail();
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json');
+          assert.equal(res.body.name, 'Cristoforo');
+          assert.equal(res.body.surname, 'Colombo');
 
           done();
         });
     });
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
+      chai
+        .request(server)
+        .keepOpen()
+        .put('/travellers')
 
-      done();
+        .send({ surname: 'da Verrazzano' })
+
+        .end(function (err, res) {
+
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json');
+          assert.equal(res.body.name, 'Giovanni');
+          assert.equal(res.body.surname, 'da Verrazzano');
+
+          done();
+        });
     });
   });
 });
 
 const Browser = require('zombie');
+Browser.site = 'http://192.168.30.194:3000'; 
 
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
+  const browser = new Browser();
 
-
+  suiteSetup(function (done) {
+    return browser.visit('/', done);
+  });
+  
 
   suite('Headless browser', function () {
     test('should have a working "site" property', function() {
